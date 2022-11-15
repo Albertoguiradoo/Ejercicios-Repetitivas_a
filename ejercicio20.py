29abdac1e228f3e257ed3a38a89c671bf47ceb7e
Alberto Guirado Méndez <al_aguiradom@iessierradelasvillas.org>
2022-11-15 14:34:13 +0100

    terminados

--- ejercicio20.py
+++ ejercicio20.py
@@ -7,25 +7,27 @@ vNUm =[]
 cantidad= 0
 primos= 0
 cantidad= int(input("¿Cúantos números primos deseas que te muestre?:\n"))
-noprimo = 0
-vNum= ()
-try:
-        num = int(input("Dime un numero: \n"))
-        for i in range(2,num):
-            if num%i==0:
-                noPrimo +=  1 
-        if noPrimo >= 1:
-            print("El número no es primo")
-        else:
-            print("El número es primo")
-except:
-    print("El programa solo admite números. Por favor, intentelo de nuevo.")
+contador  = 3
 
-def vNum():
-    
-    print(f"Los {cantidad} números primos son : {primos}")
+cantidad2 = cantidad 
+def esPrimo(num):
+    noPrimo= 0
+    for i in range(2,num):
+        if num%i==0:
+            noPrimo +=  1 
+    if noPrimo >= 1:
+        return False
+    else:
+        return True
+
+while cantidad!=0 :
+    if esPrimo(contador)== True:
+        vNUm.append(contador)
+        cantidad-=1
+
+    contador+=1 
 
 
 
-
-vNUm(1,cantidad+1)
+print(f"Los {cantidad2} primeros números primos son :")
+print(vNUm)
